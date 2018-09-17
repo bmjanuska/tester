cf1f443d6b7c6e2d812b9f58febc033f8ea11998
Bernadette Januska <bmjanuska@gmail.com>
2018-09-17 13:10:37 -0400

    Added DB for Heroku

--- server.js
+++ server.js
@@ -16,7 +16,16 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+// mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+
+var databaseUri = "mongodb://localhost/reactreadinglist";
+
+if (process.env.MONGODB_URI) {
+  mongoose.connect(process.env.MONGODB_URI); 
+} else {
+  mongoose.connect(databaseUri); 
+}
+
 
 // Start the API server
 app.listen(PORT, function() {
